2d9b6551c5240a128169db8434869016303ad863
satodaisuke1975qrl <satodaisuke.1975.qrl@gmail.com>
2023-05-29 17:24:23 +0900

    index.blade.phpを追記しました

---
+++ resources/views/tvshow/index.blade.php
@@ -0,0 +1,24 @@
+<h1>Index</h1>
+
+<table>
+    <tr>
+      <th>番号</th>
+      <th>番組名</th>
+      <th>詳細表示</th>
+    </tr>
+@foreach ($t_v_s as $tv)
+  <tr>
+    <td>{{ $tv->id }}</td>
+    <td>{{ $tv->title }}</td>
+    <td>
+        <form action="{{ route('t_v_s.show' , $tv->id) }}" method="get">
+            <button>詳細表示</button>
+        </form>
+    </td>
+  </tr>
+@endforeach
+</table>
+
+<form action="{{ route('t_v_s.create' )}}">
+    <button>新規入力</button>
+</form>

21a793cbed8ccd0e4dc2f585340f52fa8517b895
cathy831 <cathy_831@icloud.com>
2023-05-30 11:02:00 +0900

    ジャンルのテーブル・モデル・シーダーを作成しました

---
+++ database/seeders/GenresSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GenresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('genres')->insert(
+            [
+                [
+                    'genrename' =>'ドラマ/映画',
+                ],
+                [
+                    'genrename' =>'バラエティ',
+                ],
+                [
+                    'genrename' =>'音楽',
+                ],
+                [
+                    'genrename' =>'アニメ/ヒーロー',
+                ],
+                [
+                    'genrename' =>'報道/情報/ドキュメント',
+                ],
+                [
+                    'genrename' =>'スポーツ',
+                ],
+                [
+                    'genrename' =>'料理/旅/暮らし',
+                ],
+            ]);
+    }
+}

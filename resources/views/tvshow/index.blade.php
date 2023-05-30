7d01698f49df457b12b5b3a93ce0132be7c9b9d9
satodaisuke1975qrl <satodaisuke.1975.qrl@gmail.com>
2023-05-30 11:02:52 +0900

    CSS　INDEX

--- resources/views/tvshow/index.blade.php
+++ resources/views/tvshow/index.blade.php
@@ -1,24 +1,36 @@
-<h1>Index</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            index
+        </h2>
+    </x-slot>
 
-<table>
+<table class="table-auto w-full text-left whitespace-no-wrap">
+
+<thead>
     <tr>
-      <th>放送時間</th>
-      <th>番組名</th>
+      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">放送時間</th>
+      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">番組名</th>
       <th>詳細表示</th>
     </tr>
+</thead>
+<tbody>
 @foreach ($tvs as $tv)
   <tr>
-    <td>{{ $tv->time }}</td>
-    <td>{{ $tv->title }}</td>
-    <td>
+    <td class="px-4 py-3">{{ $tv->time }}</td>
+    <td class="px-4 py-3">{{ $tv->title }}</td>
+    <td class="text-center">
         <form action="{{ route('tvshow.show' , $tv->id) }}" method="get">
             <button>詳細表示</button>
         </form>
     </td>
   </tr>
 @endforeach
+</tbody>
 </table>
 
 <form action="{{ route('tvshow.create' )}}">
     <button>新規入力</button>
 </form>
+
+</x-app-layout>

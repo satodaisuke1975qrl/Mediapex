59ef5190feb954eb4e2db79609ad4643890a8a57
matsutanihhh <ichigo11421@ezweb.ne.jp>
2023-05-30 14:23:08 +0900

    管理者権限で編集可能にしたよ

--- resources/views/tvshow/show.blade.php
+++ resources/views/tvshow/show.blade.php
@@ -5,15 +5,21 @@
 <div>{{ $tv->title }}</div>
 <div>{{ $tv->content }}</div><br>
 
-<input type="button" value="編集" onclick="location.href='{{ route('tvshow.edit' , $tv->id)}}'">
+{{-- 管理者でログインした場合のみ編集ボタンを表示 --}}
+@can('admin')
+    <input type="button" value="編集" onclick="location.href='{{ route('tvshow.edit', $tv->id) }}'">
+@endcan
 
 {{-- <a href="{{ route('tvshow.edit' , $tv->id) }}">編集</a><br> --}}
-<input type="button" value="一覧に戻る" onclick="location.href='{{ route('tvshow.index')}}'">
+<input type="button" value="一覧に戻る" onclick="location.href='{{ route('tvshow.index') }}'">
 
 {{-- <a href="{{ route('tvshow.index') }}">一覧に戻る</a><br> --}}
 
-<form action="{{ route('tvshow.destroy',$tv->id)}}" method="post">
-    @csrf
-    @method('delete')
-    <input type="submit" value="削除">
-</form>
+{{-- 管理者でログインした場合のみ削除ボタンを表示 --}}
+@can('admin')
+    <form action="{{ route('tvshow.destroy', $tv->id) }}" method="post">
+        @csrf
+        @method('delete')
+        <input type="submit" value="削除">
+    </form>
+@endcan

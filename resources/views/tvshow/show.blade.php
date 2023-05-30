097b9c43f61ec8d3c2c255b014b09177f5309ec9
stoyokawa0702 <shippo72@icloud.com>
2023-05-30 11:28:15 +0900

    ボタん

--- resources/views/tvshow/show.blade.php
+++ resources/views/tvshow/show.blade.php
@@ -5,9 +5,12 @@
 <div>{{ $tv->title }}</div>
 <div>{{ $tv->content }}</div><br>
 
-<a href="{{ route('tvshow.edit' , $tv->id) }}">編集</a><br>
+<input type="button" value="編集" onclick="location.href='{{ route('tvshow.edit' , $tv->id)}}'">
 
-<a href="{{ route('tvshow.index') }}">一覧に戻る</a><br>
+{{-- <a href="{{ route('tvshow.edit' , $tv->id) }}">編集</a><br> --}}
+<input type="button" value="一覧に戻る" onclick="location.href='{{ route('tvshow.index')}}'">
+
+{{-- <a href="{{ route('tvshow.index') }}">一覧に戻る</a><br> --}}
 
 <form action="{{ route('tvshow.destroy',$tv->id)}}" method="post">
     @csrf
